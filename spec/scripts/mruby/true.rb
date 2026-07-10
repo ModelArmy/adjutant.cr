bf2501013b50ab421010048b32b076eabc16bde0
nogginly <nogginly@icloud.com>
2026-07-10 02:53:12 -0400

    Update test scripts and bring in more from mruby and comment out tests deferred to future.

---
+++ spec/scripts/mruby/true.rb
@@ -0,0 +1,39 @@
+require "assert"
+
+##
+# TrueClass ISO Test
+
+assert('TrueClass', '15.2.5') do
+  assert_equal Class, TrueClass.class
+end
+
+assert('TrueClass true', '15.2.5.1') do
+  assert_true true
+  assert_equal TrueClass, true.class
+  # --- TODO: dymanic method checking (stdlib)
+  # assert_false TrueClass.method_defined? :new
+  # ---
+end
+
+# --- TODO: operators as methods not yet supported
+# assert('TrueClass#&', '15.2.5.3.1') do
+#   assert_true true.&(true)
+#   assert_false true.&(false)
+# end
+
+# assert('TrueClass#^', '15.2.5.3.2') do
+#   assert_false true.^(true)
+#   assert_true true.^(false)
+# end
+# ---
+
+assert('TrueClass#to_s', '15.2.5.3.3') do
+  assert_equal 'true', true.to_s
+end
+
+# --- TODO: operators as methods not yet supported
+# assert('TrueClass#|', '15.2.5.3.4') do
+#   assert_true true.|(true)
+#   assert_true true.|(false)
+# end
+# ---
