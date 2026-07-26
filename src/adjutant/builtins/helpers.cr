ebab9a46fa324a62a95fac8dd910c91b40ebbfc4
nogginly <nogginly@icloud.com>
2026-07-26 02:09:23 -0400

    Add macro to help define native methods mapped to Crystal methods

--- src/adjutant/builtins/helpers.cr
+++ src/adjutant/builtins/helpers.cr
@@ -3,6 +3,21 @@ require "../native_callable"
 require "../risk_profile"
 
 module Adjutant::Builtins
+  # Use this macro to define native methods that take no arguments, do some
+  # calculation with their data and return the result
+  # - `arg_as` is the type suffix in `Value#as_XXX` methods
+  # - `return_as` is the type suffix in `Value#XXX(...)` constructor methods
+  # - `methods` is an array of native methods
+  # - `crystal_methods` is an optional array to use when the defined name doesn't match Crystal's stdlib method name
+  private macro map_self_calc_methods(arg_as, return_as, methods, crystal_methods = nil)
+    {% for method, index in methods %}
+    define(cls, interp, {{method.stringify}}) do |args|
+      val = args.first.as_{{arg_as}}
+      Adjutant::Value.{{return_as}}(val.{{crystal_methods ? crystal_methods[index] : method}})
+    end
+    {% end %}
+  end
+
   # Registers a native method on a builtin RubyClass, keyed by its
   # interned symbol id. `risk` defaults to RiskProfile.none since most
   # builtin methods (arithmetic helpers aside, which don't go through
